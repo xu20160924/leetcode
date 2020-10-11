3a82da7b438828e364949b7ce6b73849aeb91b88
xuyang <xu20151211@gmail.com>
2020-10-11 10:06:57 +0800

    cpp

--- leetcodecpp/algorithm/Morris.cpp
+++ leetcodecpp/algorithm/Morris.cpp
@@ -37,6 +37,27 @@ void inorderMorrisTraversal(TreeNode *root) {
     }
 }
 
+int main() {
+   TreeNode * root = new TreeNode(1);
+   TreeNode * two = new TreeNode(2);
+   TreeNode * seven = new TreeNode(7);
+   TreeNode * four = new TreeNode(4);
+   TreeNode * three = new TreeNode(3);
+   TreeNode * five = new TreeNode(5);
+   TreeNode * nine = new TreeNode(9);
+   TreeNode * eight = new TreeNode(8);
+
+   four->left = three;
+   four->right = five;
+   two->left = three;
+   two->right = four;
+   nine->left = eight;
+   seven->right = eight;
+   eight->left = nine;
+   root->right = seven;
+   root->left = two;
+   inorderMorrisTraversal(root);
+}
 //int main() {
 //   TreeNode * root = new TreeNode(1);
 //   TreeNode * two = new TreeNode(2);
